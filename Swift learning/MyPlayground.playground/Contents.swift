b43ee7d4f8c1eceeba7c6e0885c7b67f12f44c1f
Ethan.mo <ethan.mo@goodmonit.com>
2022-03-29 11:59:54 +0900

    Test : switch-case문에 where(추가조건문)절을 추가하여 사용해보았다.
    모든 연습 예제와 과제내용을 취합하였고, where절을 사용하였다.
    
    Closes #4

--- Swift learning/MyPlayground.playground/Contents.swift
+++ Swift learning/MyPlayground.playground/Contents.swift
@@ -1,3 +1,21 @@
+//Test1
+func printName(firstName:String, lastName:String?){
+    //if let
+    if let lName = lastName { // 옵셔널 변수가 값이 있다면 언래핑
+        print(lName,firstName)
+    }
+    else{
+        print("성이 없네요")
+    }
+
+    //guard let
+    guard let lName = lastName else{ //옵셔널 변수가 값이 없다면
+        print("성이 없네요!")
+        return
+    }
+}
+printName(firstName: "길동", lastName: "홍")
+//Test2
 func multiplyByTen(value: Int?){
     guard let number = value, number < 10 else { // 조건식이 거짓일 때 실행
         print("수가 10보다 크다")
@@ -7,7 +25,7 @@ func multiplyByTen(value: Int?){
 }
 multiplyByTen(value: 3) //30
 
-//guard문 만들기
+//Test3 guard문 만들기
 func Testfuc(a:Int?){
     guard let b = a, b > 10 else{
         print("10보다 크지 않습니다")
@@ -16,3 +34,18 @@ func Testfuc(a:Int?){
     print(a)
 }
 Testfuc(a: 8)
+//Test4 switch-case문에서 where절 사용하기
+func game(info1:Int,info2:Int){
+    
+    switch (info1){
+    case 0...19 where info2 > 50:
+        print("마나는 충분해! 체력은 부족하네?")
+    case 20...60 where info2 > 50:
+        print("마나와 체력은 충분해 보이는데?")
+    case 61...100 where info2 > 50:
+        print("거의 풀피야!")
+    default:
+        print("마나가 부족해!")
+    }
+}
+game(info1:21,info2:60)
