13db140bbc947fbad2bde493416d9b54b45d5a75
Ethan.mo <ethan.mo@goodmonit.com>
2022-03-29 11:26:15 +0900

    Feat : 나만의 guard문 만들기
    guard문은 else부터 실행하는 조건문으로,
    SWIFT의 장점 중 하나로 뽑힌다.
    자주 사용되니 꼭 익히자.
    
    closes #3

--- Swift learning/MyPlayground.playground/Contents.swift
+++ Swift learning/MyPlayground.playground/Contents.swift
@@ -1,16 +1,18 @@
-func printName(firstName:String, lastName:String?){
-    //if let
-    if let lName = lastName { // 옵셔널 변수가 값이 있다면 언래핑
-        print(lName,firstName)
-    }
-    else{
-        print("성이 없네요")
-    }
-
-    //guard let
-    guard let lName = lastName else{ //옵셔널 변수가 값이 없다면
-        print("성이 없네요!")
+func multiplyByTen(value: Int?){
+    guard let number = value, number < 10 else { // 조건식이 거짓일 때 실행
+        print("수가 10보다 크다")
         return
     }
+    print(number*10) //조건식이 참일 때 실행, 주의 : number를 여기서도 사용 가능
 }
-printName(firstName: "길동", lastName: "홍")
+multiplyByTen(value: 3) //30
+
+//guard문 만들기
+func Testfuc(a:Int?){
+    guard let b = a, b > 10 else{
+        print("10보다 크지 않습니다")
+        return
+    }
+    print(a)
+}
+Testfuc(a: 8)
