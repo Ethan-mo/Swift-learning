c7cbca8290b92fecd26e1fb87d071793270e2993
Ethan.mo <ethan.mo@goodmonit.com>
2022-04-11 23:59:49 +0900

    Test : 5. 메서드 - self의 쓰임새
    self를 언제 어떻게 사용하는지 다소 어려운 감이 있었다.
    파라미터와 해당 클래스의 프로퍼티의 이름이 같아서 햇갈리는 문제를 해결할 수 있다는 사실을
    확실하게 이해했다.
    
    Closes #40_2

--- Swift learning/MyPlayground.playground/Pages/Untitled Page 6.xcplaygroundpage/Contents.swift
+++ Swift learning/MyPlayground.playground/Pages/Untitled Page 6.xcplaygroundpage/Contents.swift
@@ -15,3 +15,31 @@ add(first: 20, second: 30)
 // second는 parameter
 
 
+// seld
+    // 현재 클래스 내 메서드나 프로퍼티를 가르킬 때 메서드나 프로퍼티 앞에 self를 붙임
+    // 아래 첫번째 소스에서는 self를 붙이거나 생략해도 된다.
+    // 두번째 소스에서는 매개변수와 구분하기 위해 반드시 써야함.
+
+// 1. self를 붙이거나 생략해도 되는 경우
+class Man{
+    var age : Int = 1
+    var weight : Double = 3.5
+    init(yourAge: Int, yourWeight : Double){
+        age = yourAge
+        weight = yourWeight
+    }
+}
+// 2. self를 반드시 붙여줘야 하는 경우
+class Man2{
+    var age : Int = 1
+    var weight : Double = 3.5
+    func display(){
+        print("나이 = \(age), 몸무게 = \(weight)")
+    }
+    init(age : Int, weight : Double){ // 여기가 다르다, 파라미터가 이름이 다르지 않고 같기 때문에, 해당 클래스 내의 프로퍼티의 변수명과 파라미터명이 동일하게되는 문제가 발생한다.
+        self.age = age
+        self.weight = weight
+    }
+}
+var kim : Man2 = Man2(age: 10, weight: 20.5)
+kim.display()
