27dea2cac8b621969510ff577bc58f0e5292ab2b
Ethan.mo <ethan.mo@goodmonit.com>
2022-04-12 18:14:52 +0900

    Swift Test : 1. App 기능 구현하기
    기본 컴포넌트인 2개의 Label과 1개의 Button,
    그리고 1개의 Textfield를 통해 간단한 앱을 제작
    
    Closes #1

--- Swift learning/ViewController.swift
+++ Swift learning/ViewController.swift
@@ -8,20 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var Hello_라벨1 : UILabel!
-    @IBOutlet var 이름_txt인풋1 : UITextField!
-    // 초반 설정으로, Nil이 기본으로 들어있는 상황이다.
-    
-    //Interface Builder Outlet(변수, 프로퍼티)
+    @IBOutlet weak var lblHello: UILabel!
+    @IBOutlet var txtName: UITextField!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         // Do Someting
     }
-    @IBAction func 버튼1(UI형태 sender: UIButton){
-        //Interface Builder Action(함수, 메서드)
-        Hello_라벨1.text = "Hello, " + 이름_txt인풋1.text!
+    
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "Hello, " + txtName.text!
     }
 }
 
