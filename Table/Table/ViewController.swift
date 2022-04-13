f786a1c1e222e80da3c6912e52fbb47782b41b7c
Ethan.mo <ethan.mo@goodmonit.com>
2022-04-13 16:24:02 +0900

    Swift Test : 5. Table View - Table View Cell Init
    테이블 뷰를 초기화해보고, 간단하게 indexPath값을 출력해주는 app을 구현
    Main Potin
    1. TableView DataSource는 필수적으로 구현해야하는 메서드가 2개가 있다.
    	1) 현재 섹션(Section)의 행(rows)은 몇개가 있는가? -> 행의 개수를 초기화 하라는 의미
    	2) 해당 행/셀 에는 어떤 있는가? -> 행/셀 을 초기화 하라는 의미
    위와같은 필수적인 메서드를 초기화를 해주어야, UITableViewDataSource 프로토콜을 채택할 수 있다.
    
    2. 시뮬레이션을 돌릴 때, 화면에 나오게 하려면?
    	1) 반드시 viewDidLoad() 과정안에, 선언을 해주어야한다.
    		So, -> table.dataSource = self, table.delegate = self
    
    Closes #5_3

--- Table/Table/ViewController.swift
+++ Table/Table/ViewController.swift
@@ -7,15 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    // UI TVD에는 필수 메서드가 없기 때문에, 채택과 동시에 아무런 위험표시가 뜨지 않는다.
+    // UI TVDS에는 필수로 구현해야하는 메서드가 있기 때문에 위험표시가 뜬다.
+    
     @IBOutlet weak var table: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        table.dataSource = self
+        table.delegate = self
     }
-
+    
+    // UI 테이블 뷰 데이터 소스 프로토콜에서 필수로 구현해야하는 메서드 1
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { //numberOfRowsInSection은 section에서 row의 개수를 의미
+        return 5 //여기가 생략이 되어있다면, 1개의 section에 1개의 row를 부여한다. row(행)와 cell(셀)은 같은 의미이다.
+    }
+    // UI 테이블 뷰 데이터 소스 프로토콜에서 필수로 구현해야하는 메서드 2
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell.init(style: .default, reuseIdentifier: "MyCell")
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
 
 }
 
