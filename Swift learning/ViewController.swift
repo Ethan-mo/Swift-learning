a0462da6c2c02a0d3bf33bc8fc2e5e1e2ed48155
Ethan.mo <ethan.mo@goodmonit.com>
2022-04-13 15:35:03 +0900

    Swift Test : 5. Table View
    Table View의 체계적인 실습을 위해
    새로운 프로젝트 Table을 새로 생성하였다.
    
    Closes #5

--- Swift learning/ViewController.swift
+++ Swift learning/ViewController.swift
@@ -15,16 +15,16 @@ class ViewController: UIViewController {
 //  뷰의 로딩이 완료 되었을 때 시스템에 의해 자동으로 호출되기 때문에, 일반적으로 리소스를 초기화하거나 초기화면을 구성하는 용도로 주로 사용. 화면이 처음 만들어질 때 한 번만 실행되므로, 처음 한 번만 실행해야하는 초기화 코드 작성
     override func viewDidLoad() {
         super.viewDidLoad()
-        // app실행과 동시에 label의 프레임 규격을 규정하는 방법
+        // UILabel을 코드로 만들기
         // 1. 한 번에 (UILabel함수를 사용)
         // let l = UILabel(frame: CGRect(x: 10, y: 100, width: 100, height: 50))
         // 2. 따로따로 (다른 속성들도 정의해주기 보기 좋음)
-        let l = UILabel()
-        l.frame = CGRect(x: 10, y: 100, width: 100, height: 50)
-        l.text = "Hello"
-        l.textColor = UIColor.red
-        l.backgroundColor = .blue   // blue라는 요소를 가져올 구조체/클래스는 'UIColor'밖에 없기때문에 가능
-        view.addSubview(l)          // 반드시 이렇게 view에 add해주어야 한다.
+        let label = UILabel()
+        label.frame = CGRect(x: 10, y: 100, width: 100, height: 50)
+        label.text = "Hello"
+        label.textColor = UIColor.red
+        label.backgroundColor = .blue   // blue라는 요소를 가져올 구조체/클래스는 'UIColor'밖에 없기때문에 가능
+        view.addSubview(label)          // 반드시 이렇게 view에 add해주어야 한다.
         
     }
 //  ViewWillAppear
