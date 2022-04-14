825456197a5f948d7578ac76667042e6fe2ad67b
Ethan.mo <ethan.mo@goodmonit.com>
2022-04-15 01:07:26 +0900

    L : Swift Project : 2. Movie Feat : 1. 배열에 있는 항목을 테이블 뷰에 표시
    기존에 제공되는 indexPath값이 아닌, 내가 지정한 값(Array)이
    구현한App에서, Click 시 Print되게끔 기능을 추가하였다.
    
    Closes #1

--- MovieHsh/MovieHsh/ViewController.swift
+++ MovieHsh/MovieHsh/ViewController.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 
+//배열에 있는 항목을 테이블 뷰에 표시
+let name = ["aaa","bbb","ccc","ddd","eee"]
+
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var table: UITableView!
@@ -23,13 +26,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
-        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! MyTableViewCell
+        cell.movieName.text = name[indexPath.row] //indexPath.description
+        return cell
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
         return 3
     }
-    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(name[indexPath.row])
+    }
 }
 
